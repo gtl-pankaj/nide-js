ce0d0ff90a1e97c86a1a89e0835cb69d5bf99e3c
gtl-pankaj <pankaj.dhiman.gtl@gmail.com>
2023-12-27 12:01:11 +0530

    created new file

--- wolf.js
+++ wolf.js
@@ -4,5 +4,10 @@
     courses:["php","java", "node"]
 }*/
 
-const arr=[" My name is Lakhan"];
-console.log(arr)
+/*const arr=[" My name is Lakhan"];
+console.log(arr)*/
+
+
+const fs = require('fs');
+fs.writeFileSync('new.txt','hello this is my new text file');
+
