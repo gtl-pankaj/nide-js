cda49fdd3b631182c67d2efa2d6bebf769c08ddc
gtl-pankaj <pankaj.dhiman.gtl@gmail.com>
2023-12-27 17:26:11 +0530

    changes in second day

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const val = require('./wolf.js');
-
-console.log(val);
+const http = require("http");
+http.createServer((req,resp) => {
+    resp.writeHead(200,{"Content-Type":"application\json"});
+    resp.write(JSON.stringify({"name":"Pankaj","last":"kumar"}));
+    resp.end();
+}).listen(2563);
