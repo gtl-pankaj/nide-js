4db8529658b6132b81fc7fd0861171ba38024a9a
gtl-pankaj <pankaj.dhiman.gtl@gmail.com>
2023-12-30 11:59:30 +0530

    ejs

---
+++ template-engine/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const dirPath = path.join(__dirname, 'inner');
+
+app.set('view engine','ejs');
+
+// app.use(express.static(dirPath));
+app.get('/profile',(req, res) => {
+    const user = {
+        name : "Pankaj",
+        email:"pankaj@gmail.com",
+        contact:1234567890,
+    }
+    res.render('profile',{user:user});
+})
+app.get('/about',(req, res) => {
+    res.sendFile(`${dirPath}/about.html`);
+})
+app.get('/contact',(req, res) => {
+    res.sendFile(`${dirPath}/contact.html`);
+})
+app.get('*',(req, res) => {
+    res.sendFile(`${dirPath}/404.html`);
+})
+app.listen(3939);
