d9bf9a828d6b3a26c5e549a176c9ce225c30170b
gtl-pankaj <pankaj.dhiman.gtl@gmail.com>
2024-01-02 12:56:46 +0530

    Mongo-Crud

--- Mongo-Crud/mongodb.js
+++ Mongo-Crud/mongodb.js
@@ -3,6 +3,7 @@ const url = "mongodb://localhost:27017";
 const databaseName = "pankaj";
 const client = new MongoClient(url);
 
+
 async function dbConnect() {
   let result = await client.connect();
   db = result.db(databaseName);
